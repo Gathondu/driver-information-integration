c77b1d3b1c4f29485d054b26fbfe576e05eec61c
Ted Maloney <Tedmaloney@me.com>
2021-08-05 11:20:47 -0400

    added controller

---
+++ app/controllers/driver_informations_controller.rb
@@ -0,0 +1,10 @@
+class DriverInformationsController < ApplicationController
+
+  def create
+    binding.pry
+  end
+
+  def show
+  end
+
+end
---
+++ test/controllers/driver_informations_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class DriverInformationsControllerTest < ActionDispatch::IntegrationTest
+
+  def test_can_retrieve_driver_information
+    payload = JSON.parse(file_fixture('example_request.json').read)
+
+    post driver_informations_url, params: payload.to_json, headers: { 'Content-Type' => 'application/json' }
+  end
+
+end

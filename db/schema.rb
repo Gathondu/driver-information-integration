e1408c7ab2800dc58ebe964e5d16645aed99a244
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2022-04-13 17:33:14 +0300

    Add information model and a model test
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_04_13_135226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "information", force: :cascade do |t|
+    t.string "api_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "license_number"
+    t.string "incidents"
+    t.string "vehicles"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end

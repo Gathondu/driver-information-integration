65e9fac59f8ed0bc3e1fde785e446001dbf4ddf9
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2022-04-13 21:54:46 +0300

    Fix serializer
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- app/serializable/serializable_information.rb
+++ app/serializable/serializable_information.rb
@@ -1,8 +1,6 @@
-module Serializable
-  class SerializableInformation < JSONAPI::Serializable::Resource
-    type 'information'
-    attribute :first_name
-    attribute :last_name
-    attribute :license_number
-  end
+class SerializableInformation < JSONAPI::Serializable::Resource
+  type 'information'
+  attribute :first_name
+  attribute :last_name
+  attribute :license_number
 end

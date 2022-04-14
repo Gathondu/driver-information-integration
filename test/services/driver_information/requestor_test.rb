241fdda512139fb49916fc4cbdb5ca505aa58491
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2022-04-14 14:49:19 +0300

    Fix faraday stub test
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- test/services/driver_information/requestor_test.rb
+++ test/services/driver_information/requestor_test.rb
@@ -4,17 +4,27 @@ require 'faraday'
 module DriverInformation
   class RequestorTest < ActiveSupport::TestCase
     test "Service returns data" do
-      stubs = Faraday::Adapter::Test::Stubs.new(strict_mode: true)
-      stubs.post(ENV['api_url'], "Authorization" => "Bearer #{ENV['auth_token']}") do |env|
-        [ 200, { 'Content-Type': 'application/json' },
-          '"{\"data\":{\"id\":\"cf5ed317235bed5a927f020b75ce8294\",\"type\":\"DriverInformation\",\"attributes\":{\"first_name\":\"Jane\",\"last_name\":\"Doe\",\"drivers_license_number\":\"00002052\",\"number_of_incidents\":\"2\",\"number_of_vehicles\":\"5\"}}}"']
+      payload = {
+        "data": {
+          "attributes": {
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "drivers_license_number": "00002052"
+          }
+        }
+      }
+      conn = Faraday.new do |builder|
+        builder.adapter :test do |stub|
+          stub.post(ENV['api_url'], payload.to_json, "Authorization" => "Bearer #{ENV['auth_token']}") do |env|
+            [ 201, { 'Content-Type': 'application/json' },
+              '{"data":{"id":"cf5ed317235bed5a927f020b75ce8294","type":"DriverInformation","attributes":{"first_name":"Jane","last_name":"Doe","drivers_license_number":"00002052","number_of_incidents":"2","number_of_vehicles":"5"}}}']
+          end
+        end
       end
-    end
+      response = conn.post(ENV['api_url'], payload.to_json, "Authorization" => "Bearer #{ENV['auth_token']}")
+      body = JSON.parse(response.body)
 
-    def client(stubs)
-      Faraday.new do |builder|
-        builder.adapter :test, stubs
-      end
+      assert_equal 201, response.status
     end
   end
 end

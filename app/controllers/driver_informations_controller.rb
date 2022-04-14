ccb6462ec5f6ca2ded4722ddeb3a198a3d45af06
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2022-04-14 11:31:13 +0300

    Add sanity check for valid license into the controller
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- app/controllers/driver_informations_controller.rb
+++ app/controllers/driver_informations_controller.rb
@@ -1,10 +1,12 @@
-class DriverInformationsController < ApplicationController
+require 'licensable'
 
+class DriverInformationsController < ApplicationController
   def create
     @params = params['driver_information']['data']['attributes']
     first_name = @params['first_name']
     last_name = @params['last_name']
     license_number = @params['drivers_license_number']
+    raise 'Invalid drivers license number' unless Licensable.check_validity(license_number)
 
     @record = Information.find_by(license_number: license_number)
     if @record

ac09d93b1343b729ea2bda392237292edd202e45
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2022-04-14 13:10:57 +0300

    Faraday mock test WIP
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- test/services/driver_information/requestor_test.rb
+++ test/services/driver_information/requestor_test.rb
@@ -1,22 +1,15 @@
 require 'test_helper'
 require 'faraday'
-require 'json'
 
 module DriverInformation
   class RequestorTest < ActiveSupport::TestCase
     test "Service returns data" do
       stubs = Faraday::Adapter::Test::Stubs.new(strict_mode: true)
-      stubs.post(ENV['api_url']) do [
-        200,
-        { 'Content-Type': 'application/json' },
-        "{\"data\":{\"id\":\"cf5ed317235bed5a927f020b75ce8294\",\"type\":\"DriverInformation\",\"attributes\":{\"first_name\":\"Jane\",\"last_name\":\"Doe\",\"drivers_license_number\":\"00002052\",\"number_of_incidents\":\"2\",\"number_of_vehicles\":\"5\"}}}"
-      ]
+      stubs.post(ENV['api_url'], "Authorization" => "Bearer #{ENV['auth_token']}") do |env|
+        [ 200, { 'Content-Type': 'application/json' },
+          '"{\"data\":{\"id\":\"cf5ed317235bed5a927f020b75ce8294\",\"type\":\"DriverInformation\",\"attributes\":{\"first_name\":\"Jane\",\"last_name\":\"Doe\",\"drivers_license_number\":\"00002052\",\"number_of_incidents\":\"2\",\"number_of_vehicles\":\"5\"}}}"']
       end
-      conn = client(stubs)
-      # FIX faraday stubs for this test
     end
-
-    private
 
     def client(stubs)
       Faraday.new do |builder|
